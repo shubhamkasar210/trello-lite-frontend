ed377ea6d694c30e0f8a7b5979c3ecc7ef1b064f
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-28 10:14:57 +0530

    basic routes and component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Projects from "./components/Projects";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
+function App() {
   return (
     <>
-      <h1>Trello App</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Projects />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
