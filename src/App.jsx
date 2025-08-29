b83ff41d10c80b726bb4e42816f97e3ae92cd071
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-29 13:32:22 +0530

    ProtectedRoute added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
@@ -17,6 +17,7 @@ import EditTask from "./components/Tasks/EditTask";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
 import AdminDashboard from "./components/AdminDashboard";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
@@ -26,36 +27,45 @@ function App() {
           <Routes>
             <Route path="/" element={<Body />}>
               <Route index element={<Login />}></Route>
-              <Route path="/projects" element={<ProjectsDashboard />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/projects" element={<ProjectsDashboard />} />
 
-              <Route path="/projects/new" element={<NewProject />} />
-              <Route path="/projects/:projectId" element={<ProjectBoard />} />
-              <Route
-                path="/projects/:projectId/edit"
-                element={<EditProject />}
-              />
+                <Route path="/projects/new" element={<NewProject />} />
+                <Route path="/projects/:projectId" element={<ProjectBoard />} />
+                <Route
+                  path="/projects/:projectId/edit"
+                  element={<EditProject />}
+                />
 
-              <Route path="projects/:projectId/tasks" element={<TaskList />} />
-              <Route
-                path="/projects/:projectId/tasks/new"
-                element={<NewTask />}
-              />
-              <Route
-                path="/projects/:projectId/tasks/:taskId"
-                element={<TaskDetails />}
-              />
-              <Route
-                path="/projects/:projectId/tasks/:taskId/edit"
-                element={<EditTask />}
-              />
+                <Route
+                  path="projects/:projectId/tasks"
+                  element={<TaskList />}
+                />
+                <Route
+                  path="/projects/:projectId/tasks/new"
+                  element={<NewTask />}
+                />
+                <Route
+                  path="/projects/:projectId/tasks/:taskId"
+                  element={<TaskDetails />}
+                />
+                <Route
+                  path="/projects/:projectId/tasks/:taskId/edit"
+                  element={<EditTask />}
+                />
 
-              <Route
-                path="/projects/:projectId/members"
-                element={<ManageMembers />}
-              />
+                <Route
+                  path="/projects/:projectId/members"
+                  element={<ManageMembers />}
+                />
 
-              <Route path="/profile" element={<Profile />}></Route>
-              <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+                <Route
+                  path="/admin-dashboard"
+                  element={<AdminDashboard />}
+                ></Route>
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Route>
             </Route>
           </Routes>
         </BrowserRouter>
---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const ProtectedRoutes = () => {
+  const user = useSelector((store) => store.user.user);
+  const location = useLocation();
+
+  if (!user && location.pathname !== "/") {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoutes;
